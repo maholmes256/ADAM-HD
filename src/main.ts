9ee1883da0c92a9fb6dc5af9b692ba6acbc15c44
Davit <davkostan@gmail.com>
2026-04-05 21:56:17 -0700

    Cleaned up startup

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,3 @@
 import bootstrap from "./bootstrapGame";
 
-const world = new Engine();
-
-(async () => {
-  // Create a new application
-  const app = new Application();
-
-  // Initialize the application
-  await app.init({ background: "#1099bb", resizeTo: window });
-
 void bootstrap();
