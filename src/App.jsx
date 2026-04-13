3ec3130f98ad1201379dc4451751dccf2fd34698
Ayesha Sabzwari <ayeshasabzwari@Ayeshas-MacBook-Air.local>
2026-04-12 19:18:30 -0700

    Dashboard UI and Problem Generation UI

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import NumberRaiders from "./components/ui/NumberRaiders";
+import ProblemGeneration from "./components/ui/ProblemOverlay";
+import { useState } from "react";
+
+export default function App() {
+  const [gameState, setGameState] = useState("MENU");
+
+  return (
+    <>
+      <NumberRaiders />
+
+      {gameState === "PROBLEM_SOLVING" && (
+        <ProblemOverlay
+          grade={1}
+          onClose={() => setGameState("PLAYING")}
+          onProblemSolved={(id, correct, grade) => {
+            console.log(id, correct, grade);
+          }}
+        />
+      )}
+    </>
+  );
+}
