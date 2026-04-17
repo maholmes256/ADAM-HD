1045aa22448d8584469a7d2c2b158862aed2293f
dfree-5432 <destiny.freeman@sjsu.edu>
2026-04-17 13:42:46 -0700

    Finished all test cases for generator.ts

--- tests/services.test/mathProblem.test/generator.test.ts
+++ tests/services.test/mathProblem.test/generator.test.ts
@@ -1,4 +1,4 @@
-import { assertType, describe, expectTypeOf, it, expect } from 'vitest'
+import { assertType, describe, expectTypeOf, it, expect, vitest } from 'vitest'
 import { generateProblem } from '../../../src/services/mathProblem/generator.ts'
 import { GRADE_TO_SPEC } from "../../../src/services/mathProblem/specMap";
 import { Expression, GenSpec, MathProblem } from "../../../src/services/mathProblem/types";
@@ -25,18 +25,51 @@ describe("generateProblem check grade", () => {
 
 describe("generateProblem check equation", () => {
   it.each ([
-    [1, 11],
-    [2,21],
-    [3,31],
-  ]) ("generateProblem(%i) should return with an appropriate operation value", (a, expected) => {
+    1,
+    2,
+    3,
+  ]) ("generateProblem(%i) should return with an appropriate operation value", (a) => {
       
     const p = generateProblem(a);  
-    console.log(p);
       // @ts-ignore
     expect(p.expr?.op).toBeOneOf(['+', '-', '*', '/']);
       // @ts-ignore
-    console.log(p.expr?.op);
     expect(p.problemId).toBeTypeOf("string");
+    expect(p.problemId).not.toBeNull;
+
 
   }); 
 }); 
+
+
+describe("generateProblem call", () => {
+  const generated = {
+  generateNewProblem(grade: number) {
+    return generateProblem(grade).gradeLevel;
+  },
+  }
+
+  it.each ([
+    1,
+    2,
+    3,
+
+  ]) ("generateProblem(%i) should be appropriately called", (a) => {
+    
+    const generateProblemSpy = vitest.spyOn(generated, 'generateNewProblem');
+
+    const gradeTest = generated.generateNewProblem(a);
+//Checks that generateProblem(a) has been appropriately returned, and called
+    expect(gradeTest).toBe(a);
+    expect(generateProblemSpy).toHaveBeenCalledWith(a);
+    expect(generateProblemSpy).toHaveReturned;
+//Checks that 
+    generated.generateNewProblem(3);
+    generated.generateNewProblem(1);
+    generated.generateNewProblem(2);
+
+    expect(generateProblemSpy).toHaveBeenLastCalledWith(2);
+    expect(generateProblemSpy).toHaveNthReturnedWith(3, 1);
+   
+  }); 
+});
