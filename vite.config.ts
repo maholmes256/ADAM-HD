12ff92ad8a5e2fe6a5caa36b468799d66e3cd350
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2026-05-01 12:56:19 -0700

    Added .env prefixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  envPrefix: ["VITE_", "FIREBASE_"],
   server: {
     port: 8080,
     open: true,
