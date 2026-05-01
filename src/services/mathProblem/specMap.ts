ec47a052b7d584f3443a96cfa466cdb57fc2fa27
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2026-05-01 13:53:14 -0700

    Added specs for grades 4 and 5

--- src/services/mathProblem/specMap.ts
+++ src/services/mathProblem/specMap.ts
@@ -2,7 +2,9 @@ import { GenSpec } from "./types";
 
 // Record is the same as Map but it is optimized for objects known before run-time
 export const GRADE_TO_SPEC: Record<number, GenSpec> = {
-  1: {ops: ['+'], maxNum: 10, maxDepth: 1},
-  2: {ops: ['+', '-'], maxNum: 30, maxDepth: 1},
-  3: {ops: ['+', '-', '*'], maxNum: 40, maxDepth: 2}
+  1: {ops: ['+', '-'], maxNum: 20, maxDepth: 1},
+  2: {ops: ['+', '-'], maxNum: 99, maxDepth: 1},
+  3: {ops: ['+', '-', '*'], maxNum: 999, maxMultNum: 12, maxDepth: 1},
+  4: {ops: ['+', '-', '*'], maxNum: 9999, maxMultNum: 99, maxDepth: 1},
+  5: {ops: ['+', '-', '*'], maxNum: 12, maxDepth: 2, minDepth: 2}
 };
